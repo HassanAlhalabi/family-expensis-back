688251dabcd86255c0e8005f0fe47a2968232dd0
Hassan Alhalabi <hassan@petabayte.com>
2022-10-15 12:36:21 +0300

    Fix DB Connection

--- db/index.js
+++ db/index.js
@@ -1,22 +1,20 @@
 const mysql = require('mysql');
 
-const connectDB = async () => {
-  
-  const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-  }); // Recreate the connection, since
-                                                  // the old one cannot be reused.
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
 
-  connection.connect(function(err) {              // The server is either down
-    if(err) {                                     // or restarting (takes a while sometimes).
-      console.log('error when connecting to db:', err);
-      setTimeout(connectDB, 2000); // We introduce a delay before attempting to reconnect,
-    }                                     // to avoid a hot loop, and to allow our node script to
-  });                                     // process asynchronous requests in the meantime.
-                                          // If you're also serving http, display a 503 error.
+const connectDB = async () => {
+  try {
+    connection.connect();
+    console.log('Connected Successfully')
+  } catch(error) {
+    console.log('Failed To Connect')
+  }
+
   connection.on('error', function(err) {
     console.log('db error', err);
     if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
@@ -25,6 +23,7 @@ const connectDB = async () => {
       throw err;                                  // server variable configures this)
     }
   });
+
 }   
 
 const query = async (sql) => {
